e458c0e5c128dd157808d10dcbc23fd50b8daac8
UbaMarvin <ubamarvin95@gmail.com>
2024-04-04 15:53:37 +0200

    refactored main -> Hangman.scala , created test for createEmptyBottomRow

--- build.sbt
+++ build.sbt
@@ -5,8 +5,7 @@ lazy val root = project
   .settings(
     name := "SE-Project",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.18",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.18" % "test"
   )
---
+++ src/test/scala/HangmanSpec.scala
@@ -0,0 +1,31 @@
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class HangmanSpec extends AnyWordSpec:
+  "Hangman" should {
+
+
+    "have a scalable bottomRow " in {
+      createEmptyBottomRow(1) should be("_ " + eol)
+      createEmptyBottomRow(2) should be("_ _ " + eol)
+      createEmptyBottomRow(3) should be("_ _ _ " + eol)
+    }
+/* 
+    "have a scalable cell " in {
+      cells(1, 1) should be("| |" + eol)
+      cells(1, 2) should be("| | |" + eol)
+      cells(2, 1) should be("|  |" + eol)
+    }
+
+    "hava a cell as String in form '|   |   |   |'" in {
+      cells() should be("|   |   |   |" + eol)
+    }
+
+    "have a mash in the form " +
+      "+-+ " +
+      "| |" +
+      "+-+" in {
+        mesh(1, 1) should be("+-+" + eol + "| |" + eol + "+-+" + eol)
+      }
+      */
+  }
