cb546ad60584a37b62661f267dd1aa3562aba6fa
KadirHsnvc <164490881+KadirHsnvc@users.noreply.github.com>
2024-04-12 00:08:26 +0200

    added a comment

--- src/main/scala/de/htwg/se/Hangman/Hangman.scala
+++ src/main/scala/de/htwg/se/Hangman/Hangman.scala
@@ -1,50 +1,88 @@
 import scala.util.Random
 
-val words = Array("Ei", "Ein", "Vier", "Leben", "Lebend", "Gorilla")
-val random = new Random()
-val secretWord = words(random.nextInt(words.length))
-val eol = sys.props("line.separator")
-val gameFieldStartRow5 = "               " + eol
-val gameFieldStartRow4 = "               " + eol
-val gameFieldStartRow3 = "               " + eol
-val gameFieldStartRow2 = "               " + eol
-val gameFieldStartRow1 = "               " + eol
+object HangmanGame {
+  val words = Array("Typ", "Recycling", "Versicherung", "Uni", "Gymnastik", "Überraschungsei", "Xylophon")
+  val random = new Random()
+  val secretWord = words(random.nextInt(words.length)).toUpperCase()
 
-//Starting point of the Game
-@main
-def hangMan(): Unit =
-  println(createEmptyGameField(secretWord.length()))
-  println(findCharPositionInSecretWord("ABA", 'A').mkString)
-  
-
-def createEmptyBottomRow(lenghtOfword: Int): String = 
-    val emptyBottomRow = "_ "
-    emptyBottomRow * lenghtOfword + eol
-
-def createEmptyGameField(lenghtOfword: Int) = 
-  gameFieldStartRow5 + gameFieldStartRow4 + gameFieldStartRow3 + gameFieldStartRow2 + gameFieldStartRow1 + createEmptyBottomRow(
-    lenghtOfword
+  val hangmanArt = Array(
+    "  __",
+    " |    |",
+    " |",
+    " |",
+    " |",
+    "|"
   )
 
-//Perhaps position should be Array since letters can cccur more than once
-//however i dont know how to store numOfOccurences in this fascist functional context
-//maybe i can pass a function that returns all the fkn positions?
+  val emptyBottomRow = "_ "
 
-//this is bullshit, because it only works on the first go, again i need to somehow save the changed state of the bottom row
-def manipulateBottomRow(func: (String, Char) => Array[Int], letter: Char, lenghtOfword: Int): String =
-  "Bullshit"
+  val gameFieldStartRow5 = "               " + sys.props("line.separator")
+  val gameFieldStartRow4 = "               " + sys.props("line.separator")
+  val gameFieldStartRow3 = "               " + sys.props("line.separator")
+  val gameFieldStartRow2 = "               " + sys.props("line.separator")
+  val gameFieldStartRow1 = "               " + sys.props("line.separator")
 
-//Untested
-// I need this to figure out where the playerGuess letters are located to update them int the field
-def findCharPositionInSecretWord(secretWord: String, playerGuess: Char): Array[Int] =
-  secretWord.zipWithIndex.
-              collect{case (c, i) if c == playerGuess => i}.toArray
+  def createEmptyBottomRow(lengthOfWord: Int): String = 
+    emptyBottomRow * lengthOfWord + sys.props("line.separator")
 
-def updateGameField(): String =
-  "Unimplemented"
+  def createEmptyGameField(lengthOfWord: Int): String = 
+    gameFieldStartRow5 + gameFieldStartRow4 + gameFieldStartRow3 + gameFieldStartRow2 + gameFieldStartRow1 + createEmptyBottomRow(lengthOfWord)
 
-  //println(createEmptyGameField(secretWord.length()))
+  def readPlayerGuess(): Char = {
+    println("Guess a letter:")
+    scala.io.StdIn.readLine().toUpperCase().head
+  }
 
+  def updateGameField(wrongAttempts: Int, guessedLetters: Set[Char]): String = {
+    val updatedGameField = hangmanArt.clone()
 
-//kadir
+    // Draw the Hangman figure based on the number of wrong attempts
+    wrongAttempts match {
+      case 1 =>
+        updatedGameField(2) = " |    O"
+      case 2 =>
+        updatedGameField(2) = " |    O"
+        updatedGameField(3) = " |    |"
+      case 3 =>
+        updatedGameField(2) = " |    O"
+        updatedGameField(3) = " |   /|"
+      case 4 =>
+        updatedGameField(2) = " |    O"
+        updatedGameField(3) = " |   /|\\"
+      case 5 =>
+        updatedGameField(2) = " |    O"
+        updatedGameField(3) = " |   /|\\"
+        updatedGameField(4) = " |   /"
+      case 6 =>
+        updatedGameField(2) = " |    O"
+        updatedGameField(3) = " |   /|\\"
+        updatedGameField(4) = " |   / \\"
+      case _ => // Do nothing for correct guesses
+    }
 
+    // Fill in guessed letters in the bottom row
+    val filledBottomRow = secretWord.map { char =>
+      if (guessedLetters.contains(char)) char else '_'
+    }.mkString(" ") + sys.props("line.separator")
+
+    updatedGameField.mkString(sys.props("line.separator")) + sys.props("line.separator") + filledBottomRow
+  }
+
+  def hangmanGame(wrongAttempts: Int, guessedLetters: Set[Char]): Unit = {
+    if (secretWord.toSet.subsetOf(guessedLetters)) {
+      println("Congratulations! You guessed the word correctly.")
+    } else if (wrongAttempts >= 6) {
+      println("Sorry, you lost. The word was: " + secretWord)
+    } else {
+      println(updateGameField(wrongAttempts, guessedLetters))
+      val guess = readPlayerGuess()
+      val updatedGuesses = guessedLetters + guess
+      val updatedAttempts = if (secretWord.contains(guess)) wrongAttempts else wrongAttempts + 1
+      hangmanGame(updatedAttempts, updatedGuesses)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    hangmanGame(0, Set.empty)
+  }
+}
