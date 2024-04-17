36edaa4a1be80c1e6caf64c90c1b2e495dfefd66
UbaMarvin <ubamarvin95@gmail.com>
2024-04-17 16:03:50 +0200

    HangmanSpec.Scala reformatted, add test for printHell

--- src/test/scala/HangmanSpec.scala
+++ src/test/scala/HangmanSpec.scala
@@ -1,37 +1,12 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
+import HangmanGame.printHello
 
 class HangmanSpec extends AnyWordSpec:
   "Hangman" should {
 
 
-    "have a scalable bottomRow " in {
-      createEmptyBottomRow(1) should be("_ " + eol)
-      createEmptyBottomRow(2) should be("_ _ " + eol)
-      createEmptyBottomRow(3) should be("_ _ _ " + eol)
+    "say hello" in {
+      printHello() should be ("hello")
     }
- 
-    "have a scalable gameField " in {
-      createEmptyGameField(1) should be("               " + eol
-                                       +"               " + eol
-                                       +"               " + eol
-                                       +"               " + eol
-                                       +"               " + eol
-                                       +"_ " + eol)
-                                      
-
-
-    }
-    /*
-    "hava a cell as String in form '|   |   |   |'" in {
-      cells() should be("|   |   |   |" + eol)
-    }
-
-    "have a mash in the form " +
-      "+-+ " +
-      "| |" +
-      "+-+" in {
-        mesh(1, 1) should be("+-+" + eol + "| |" + eol + "+-+" + eol)
-      }
-      */
   }
